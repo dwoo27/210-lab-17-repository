7b0b46534f475055e24a8c01b1952b549ec36e29
dwoo27 <dwooed27@gmail.com>
2026-03-05 21:13:36 -0800

    10 min commit

--- 210-lab-17.cpp
+++ 210-lab-17.cpp
@@ -1,20 +1,111 @@
-// 210-lab-17.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+const int SIZE = 7;
+struct Node {
+	float value;
+	Node* next;
+};
+void output(Node*);
+void addToFront(Node*& head, float val);
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	Node* head = nullptr;
+	int count = 0;
+	// create a linked list of size SIZE with random numbers 0-99
+	for (int i = 0; i < SIZE; i++) {
+		int tmp_val = rand() % 100;
+		addToFront(head, tmp_val);
+		
+	output(head);
+	// deleting a node
+	cout << "Which node to delete? " << endl;
+	output(head);
+	int entry;
+	cout << "Choice --> ";
+	cin >> entry;
+	// traverse that many times and delete that node
+	Node* current = head;
+	Node* prev = nullptr; // start prev as nullptr to detect head deletion
+	for (int i = 0; i < (entry - 1); i++) {
+		prev = current;
+		current = current->next;
+	}
+	// at this point, delete current and reroute pointers
+	if (current) {
+		if (prev == nullptr) {
+			// deleting the head node
+			head = current->next;
+		}
+		else {
+			prev->next = current->next;
+		}
+		delete current;
+		current = nullptr;
+	}
+	output(head);
+	// insert a node
+	cout << "After which node to insert 10000? " << endl;
+	count = 1;
+	current = head;
+	while (current) {
+		cout << "[" << count++ << "] " << current->value << endl;
+		current = current->next;
+	}
+	cout << "Choice --> ";
+	cin >> entry;
+	current = head;
+	prev = nullptr; // reset prev to nullptr for same reason
+	for (int i = 0; i < entry; i++) {
+		prev = current;
+		current = current->next;
+	}
+	// at this point, insert a node between prev and current
+	Node* newnode = new Node;
+	newnode->value = 10000;
+	newnode->next = current;
+	if (prev == nullptr) {
+		// inserting before the head
+		head = newnode;
+	}
+	else {
+		prev->next = newnode;
+	}
+	output(head);
+	// deleting the linked list
+	current = head;
+	while (current) {
+		head = current->next;
+		delete current;
+		current = head;
+	}
+	head = nullptr;
+	output(head);
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void output(Node* hd) {
+	if (!hd) {
+		cout << "Empty list.\n";
+		return;
+	}
+	int count = 1;
+	Node* current = hd;
+	while (current) {
+		cout << "[" << count++ << "] " << current->value << endl;
+		current = current->next;
+	}
+	cout << endl;
+}
+void addToFront(Node*& head, int val){
+	Node* newVal = new Node;
+	// adds node at head
+	if (!head) {
+		head = newVal;
+		newVal->next = nullptr;
+		newVal->value = tmp_val;
+	}
+	else {
+		newVal->next = head;
+		newVal->value = tmp_val;
+		head = newVal;
+	}
+}
